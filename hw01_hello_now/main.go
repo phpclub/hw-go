8879d7f3e8ae7c80e6a3e524edb9af947019dcbd
Alexandr Smirnov <phpclub@gmail.com>
2020-03-14 16:17:02 +0300

    hw01 Реализован вывод текущего времени и получение его через сервер NTP

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	const ctLabel = "current time:"
+	const ntpLabel = "exact time:"
+	const ntpHost = "0.beevik-ntp.pool.ntp.org"
+	cTime := time.Now()
+	//Зададим формат - каким надо быть альтернативно одаренным чтоб придумать такое
+	cFormattedTime := cTime.UTC().Format("2006-01-02 15:04:05 -0700 MST")
+	fmt.Println(ctLabel, cFormattedTime)
+	// Получим сетевое время из ntpHost
+	ntpTime, ntpErr := ntp.Time(ntpHost)
+	if ntpErr != nil {
+		log.Fatalln("NTP Error:", ntpErr)
+	}
+	cFormattedNtpTime := ntpTime.UTC().Format("2006-01-02 15:04:05 -0700 MST")
+	fmt.Println(ntpLabel, cFormattedNtpTime)
 }
