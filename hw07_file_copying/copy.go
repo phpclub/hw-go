a703e9cf2bb3d6839d5e0d1c2b8729bf2afc9c96
Alexandr Smirnov <phpclub@gmail.com>
2020-05-24 14:46:21 +0300

    hw07_file_copying done

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,77 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrMissingParamFrom      = errors.New("missing input -from")
+	ErrMissingParamTo        = errors.New("missing output -to")
 )
 
+const tmpl = `{{ red "Process:" }} {{ bar . "[" "*" (cycle . "↖" "↗" "↘" "↙" ) "." "]" | green }} {{speed . | green }} {{percent . | red }}`
+
 func Copy(fromPath string, toPath string, offset, limit int64) error {
-	// Place your code here
+	if fromPath == "" {
+		return ErrMissingParamFrom
+	}
+	if toPath == "" {
+		return ErrMissingParamTo
+	}
+	// Обработаем fileFrom
+	fileFrom, err := os.OpenFile(fromPath, os.O_RDONLY, 0)
+	if err != nil {
+		return err
+	}
+	defer fileFrom.Close()
+	fileFromStat, err := fileFrom.Stat()
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	if fileFromStat.Size() == 0 {
+		return ErrUnsupportedFile
+	}
+	if offset > fileFromStat.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+	if limit == 0 {
+		limit = fileFromStat.Size()
+	} else if limit > fileFromStat.Size() {
+		limit = fileFromStat.Size()
+	}
+	// Обработаем fileTo
+	fileTo, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer fileTo.Close()
+
+	if offset > 0 {
+		//Перемотаем позицию файла
+		_, err := fileFrom.Seek(offset, 0)
+		if err != nil {
+			return ErrOffsetExceedsFileSize
+		}
+	}
+
+	reader := io.LimitReader(fileFrom, limit)
+	// start new bar
+	bar := pb.ProgressBarTemplate(tmpl).Start64(limit)
+	// create proxy reader
+	barReader := bar.NewProxyReader(reader)
+	//if offset == 0 {
+	written, err := io.CopyN(fileTo, barReader, limit)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Copyng: %d bytes\n", written)
+	// finish bar
+	bar.Finish()
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,91 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// Удаляем файлы сгенеренные тестами.
+func cleanUp(file string) {
+	err := os.Remove(file)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Получим размер файла для тестов.
+func getFileSize(file string) int64 {
+	iFile, err := os.OpenFile(file, os.O_RDONLY, 0)
+	if err != nil {
+		panic(err)
+	}
+	defer iFile.Close()
+	iFileStat, err := iFile.Stat()
+	if err != nil {
+		panic(err)
+	}
+	return iFileStat.Size()
+}
 
 func TestCopy(t *testing.T) {
-	// Place your code here
+	path, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	testInputFile := path + "/testdata/input.txt"
+	testOutputFile := path + "/test_output.txt"
+	testOutputSeekFile := path + "/testdata/out_offset100_limit1000.txt"
+
+	t.Run("limit 0", func(t *testing.T) {
+		ok := Copy(testInputFile, testOutputFile, 0, 0)
+		require.NoError(t, ok)
+		require.Equal(t, getFileSize(testInputFile), getFileSize(testOutputFile))
+		cleanUp(testOutputFile)
+	})
+
+	t.Run("limit 10", func(t *testing.T) {
+		ok := Copy(testInputFile, testOutputFile, 0, 10)
+		require.NoError(t, ok)
+		require.Equal(t, int64(10), getFileSize(testOutputFile))
+		cleanUp(testOutputFile)
+	})
+
+	t.Run("limit 10000", func(t *testing.T) {
+		ok := Copy(testInputFile, testOutputFile, 0, 10000)
+		require.NoError(t, ok)
+		require.Equal(t, getFileSize(testInputFile), getFileSize(testOutputFile))
+		cleanUp(testOutputFile)
+	})
+
+	t.Run("offset 100 limit 1000", func(t *testing.T) {
+		ok := Copy(testInputFile, testOutputFile, 100, 1000)
+		require.NoError(t, ok)
+		require.Equal(t, int64(1000), getFileSize(testOutputFile))
+		//Сравним testOutputSeekFile и testOutputFile
+		outSeek, err := ioutil.ReadFile(testOutputSeekFile)
+		if err != nil {
+			panic(err)
+		}
+		outFile, err := ioutil.ReadFile(testOutputFile)
+		if err != nil {
+			panic(err)
+		}
+		require.Equal(t, outSeek, outFile)
+		cleanUp(testOutputFile)
+	})
+
+	t.Run("offset 7000", func(t *testing.T) {
+		ok := Copy(testInputFile, testOutputFile, 7000, 0)
+		require.Error(t, ok)
+		require.EqualError(t, ok, "offset exceeds file size")
+	})
+
+	t.Run("Unsupported File", func(t *testing.T) {
+		ok := Copy("/dev/urandom", testOutputFile, 7000, 0)
+		require.Error(t, ok)
+		require.EqualError(t, ok, "unsupported file")
+	})
 }
