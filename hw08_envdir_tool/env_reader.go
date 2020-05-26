c0a2172bb69b0f2420e477c592903d6a874a338a
Alexandr Smirnov <phpclub@gmail.com>
2020-05-26 19:44:05 +0300

    DRAFT

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,10 +1,51 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
 type Environment map[string]string
 
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return Environment{}, fmt.Errorf("failed reading directory: %s", dir)
+	}
+	for _, file := range files {
+		//fmt.Println(file.Name())
+		// Получим первую строку из файла
+		content, err := ReadLineFromFile(dir + file.Name())
+		if err != nil {
+			return Environment{}, fmt.Errorf("failed read file: %s", file.Name())
+		}
+		fmt.Println("file:" + file.Name() + "\nContent:" + content + "#\n")
+	}
+	t, ok := os.LookupEnv("TERMINAL_EMULATOR")
+	fmt.Println(t, ok)
+	if ok {
+		os.Setenv("TERMINAL_EMULATOR", "hacked ttys")
+	}
+	t, ok = os.LookupEnv("TERMINAL_EMULATOR")
+	fmt.Println(t, ok)
 	return nil, nil
+}
+
+func ReadLineFromFile(path string) (str string, err error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+	sc := bufio.NewScanner(f)
+	for sc.Scan() {
+		str = sc.Text()
+		err = sc.Err()
+		return
+	}
+	return "", err
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,60 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+var (
+	ErrMissingDestinationDir = errors.New("missing destination dir")
+	ErrMissingComand         = errors.New("missing command to execute")
+)
+
+const requiredArgs = 2
+
+// получение аргументов из командной строки.
+func parseArgs() (dir string, command string, args map[int]string, err error) {
+	// Проверим на наличие обязательных аргументов
+	incomingArgs := os.Args[1:]
+	if len(incomingArgs) >= requiredArgs {
+		if len(incomingArgs[0]) > 0 {
+			dir = incomingArgs[0]
+		}
+		if len(incomingArgs[1]) > 0 {
+			command = incomingArgs[1]
+		}
+	} else {
+		switch len(incomingArgs) {
+		case 0:
+			err = ErrMissingDestinationDir
+		case 1:
+			err = ErrMissingComand
+		}
+		return dir, command, args, err
+	}
+	if len(incomingArgs[2:]) > 0 {
+		// Создадим map c переданными аргументами
+		args = make(map[int]string, len(incomingArgs[2:]))
+		for k, v := range incomingArgs[2:] {
+			args[k] = v
+		}
+	}
+	return dir, command, args, err
+}
+
 func main() {
-	// Place your code here
+	dir, cmd, args, err := parseArgs()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println(dir, cmd, args)
+	cmdEnv, err := ReadDir(dir)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println(cmdEnv)
+
 }
